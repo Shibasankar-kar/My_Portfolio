f02ce6bdfb6d85333ca9973a6e0d573ca7e7d71e
shibasankar kar <shibasankardev@gmail.com>
2025-05-03 15:36:36 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		preview: {
-			allowedHosts: ['shibasankar.life'],
-		},
+		allowedHosts: ['shibasankar.life'],
 	},
 });
