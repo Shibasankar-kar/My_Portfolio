3a8d7db50551ac6af29e562ecb4a398cb4e592d6
shibasankar kar <shibasankardev@gmail.com>
2025-05-06 06:19:58 +0530

    added style and global tailwind css

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-	return <div className="text-2xl font-bold">App</div>;
-}
-
-export default App;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+	return (
+		<section
+			id="home"
+			className="border border-red-500 h-dvh relative text-white-50 px-5"
+		>
+			<div className="h-full w-full flex-center">
+				<div className="container relative w-full h-full"></div>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
